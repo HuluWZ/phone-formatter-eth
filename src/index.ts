4a8c3c9d54884025b9aedff191da99a24c7875aa
HuluWZ <hulunlante.w@gmail.com>
2024-08-31 10:07:59 +0300

    feat: Improve phone number formatting logic, add type parameter to formatPhone function, and introduce isValid function

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,55 @@
-export function formatPhone(phone: string) {
-  const phone_length = phone?.toString().length;
-  if (phone_length === 13 && phone.startsWith("+251")) {
-    return phone;
-  } else if (phone_length === 12 && phone.startsWith("251")) {
-    return `+${phone}`;
-  } else if (phone_length === 10 && ["09", "07"].includes(phone.slice(0, 2))) {
-    return `+251${phone.slice(1)}`;
-  } else if (phone_length === 9 && ["9", "7"].includes(phone.charAt(0))) {
-    return `+251${phone}`;
+/**
+ *
+ * @param phone - phone number to be formatted
+ * @param type -  local or international  by default it is international
+ * @returns formatted phone number it it's valid otherwise it returns "INVALID_PHONE_NUMBER"
+ */
+export function formatPhone(
+  phone: string,
+  type: "local" | "international" = "international"
+) {
+  const formatted_phone = phone.replace(/[^+\d]/g, "");
+  const phone_length = formatted_phone?.toString().length;
+  if (phone_length < 9 || phone_length > 13) {
+    return "INVALID_PHONE_NUMBER";
+  } else if (phone_length === 13 && formatted_phone.startsWith("+251")) {
+    return type === "local" ? formatLocal(formatted_phone) : formatted_phone;
+  } else if (phone_length === 12 && formatted_phone.startsWith("251")) {
+    return type === "local"
+      ? formatLocal(`+${formatted_phone}`)
+      : `+${formatted_phone}`;
+  } else if (
+    phone_length === 10 &&
+    ["09", "07"].includes(formatted_phone.slice(0, 2))
+  ) {
+    return type === "local"
+      ? formatLocal(`+251${formatted_phone.slice(1)}`)
+      : `+251${formatted_phone.slice(1)}`;
+  } else if (
+    phone_length === 9 &&
+    ["9", "7"].includes(formatted_phone.charAt(0))
+  ) {
+    return type === "local"
+      ? formatLocal(`+251${formatted_phone}`)
+      : `+251${formatted_phone}`;
   } else {
-    return "INVALID PHONE NUMBER";
+    return "INVALID_PHONE_NUMBER";
   }
 }
+/**
+ *
+ * @param phone - phone number to be formatted
+ * @returns phone number formatted to local format like 09 or 07
+ */
+export function formatLocal(phone: string) {
+  return phone?.replace("+251", "0");
+}
 
+/**
+ *
+ * @param phone - phone number to check the operator
+ * @returns 'Safaricom' or 'Ethio Telecom' if it's valid otherwise it returns "UNKNOWN"
+ */
 export function checkOperator(phone: string) {
   const formattedPhone = formatPhone(phone);
   if (formattedPhone.startsWith("+2519")) {
@@ -22,4 +59,26 @@ export function checkOperator(phone: string) {
   } else {
     return "UNKNOWN";
   }
+}
+/**
+ *
+ * @param phone - phone number to check if it's valid
+ * @returns true if it's valid otherwise it returns false
+ */
+export function isValid(phone: string) {
+  const formattedPhone = formatPhone(phone);
+  return formattedPhone === "INVALID_PHONE_NUMBER" ? false : true;
+}
+
+/**
+ * Parses the phone number to remove '-', '(', and ')'.
+ * @param phone - The phone number to be cleaned.
+ * @returns The cleaned phone number if it's valid otherwise "INVALID_PHONE_NUMBER".
+ */
+export function parse(phone: string): string {
+  const validPhoneRegex = /^[0-9-()\s]+$/;
+  if (!validPhoneRegex.test(phone)) {
+    return "INVALID_PHONE_NUMBER";
+  }
+  return phone.replace(/[-() ]/g, "");
 }
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,18 +1,18 @@
-import { formatPhone, checkOperator } from "../src/index";
+import { formatPhone, checkOperator, isValid, parse } from "../src/index";
 
 describe("formatPhone", () => {
   test("should format phone numbers correctly", () => {
     // Test with different formats and expected outputs
     expect(formatPhone("0912345678")).toBe("+251912345678"); // Standard 10-digit format
-    expect(formatPhone("+251912345678")).toBe("+251912345678"); // Already formatted number
-    expect(formatPhone("251912345678")).toBe("+251912345678"); // Missing '+'
+    expect(formatPhone("+251912345678", "local")).toBe("0912345678"); // Already formatted number
+    expect(formatPhone("251912345678", "local")).toBe("0912345678"); // Missing '+'
     expect(formatPhone("0912345678")).toBe("+251912345678"); // 9-digit number, prefix '09'
     expect(formatPhone("0712345678")).toBe("+251712345678"); // Incorrect format, treated as unknown
-    expect(formatPhone("912345678")).toBe("+251912345678"); // 9-digit  ethio-tel number, missing '+251'
+    expect(formatPhone("912345678", "local")).toBe("0912345678"); // 9-digit  ethio-tel number, missing '+251'
     expect(formatPhone("712345678")).toBe("+251712345678"); // 9-digit  safari number, missing '+251'
-    expect(formatPhone("1234567890")).toBe("INVALID PHONE NUMBER"); // Invalid Phone number
-    expect(formatPhone("812345678")).toBe("INVALID PHONE NUMBER"); // Invalid Phone number
-    expect(formatPhone("")).toBe("INVALID PHONE NUMBER"); // Empty input
+    expect(formatPhone("1234567890", "local")).toBe("INVALID_PHONE_NUMBER"); // INVALID_PHONE_NUMBER
+    expect(formatPhone("812345678")).toBe("INVALID_PHONE_NUMBER"); // INVALID_PHONE_NUMBER
+    expect(formatPhone("")).toBe("INVALID_PHONE_NUMBER"); // Empty input
   });
 });
 
@@ -29,5 +29,32 @@ describe("checkOperator", () => {
     expect(checkOperator("1234567890")).toBe("UNKNOWN"); // Invalid Phone Number input
     expect(checkOperator("812345678")).toBe("UNKNOWN"); // Invalid Phone Number input
     expect(checkOperator("")).toBe("UNKNOWN"); // Empty input
+  });
+});
+
+describe("isValid", () => {
+  test("should identify if phone number is valid or not correctly", () => {
+    // Test with different formatted numbers and expected operators
+    expect(isValid("0912345678")).toBe(true); // Standard 10-digit format
+    expect(isValid("+251912345678")).toBe(true); // Already formatted number
+    expect(isValid("+251712345678")).toBe(true); // Already formatted number
+    expect(isValid("251912345678")).toBe(true); // Number starting with '07'
+    expect(isValid("0912345678")).toBe(true); // Missing '+2519'
+    expect(isValid("0712345678")).toBe(true); // Missing prefix `+2517`
+    expect(isValid("712345678")).toBe(true); // 9-digit number
+    expect(isValid("1234567890")).toBe(false); // Invalid Phone Number input
+    expect(isValid("812345678")).toBe(false); // Invalid Phone Number input
+    expect(isValid("")).toBe(false); // Empty input
+  });
+});
+
+describe("parse", () => {
+  test("should parse phone numbers correctly", () => {
+    expect(parse("(251) 911-123-456")).toBe("251911123456"); // Standard 10-digit format
+    expect(parse("(251) 9-11-123-456")).toBe("251911123456"); // Standard 10-digit format
+    expect(parse("(251) 9111-23-456")).toBe("251911123456"); // Standard 10-digit format
+    expect(parse("(251) 911-123-456")).toBe("251911123456"); // Standard 10-digit format
+    expect(parse("(2519) 11-123-456")).toBe("251911123456"); // Standard 10-digit format
+    expect(parse("")).toBe("INVALID_PHONE_NUMBER"); // Empty input
   });
 });
